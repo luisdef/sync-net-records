b50d97182b2b1829a1a2779ac1a9eb295ce42655
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-08-06 11:39:13 -0300

    text utilities to generate and format text

---
+++ utils/text.py
@@ -0,0 +1,42 @@
+import sys
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def trim(docstring):
+    if not docstring:
+        return ''
+    lines = docstring.expandtabs().splitlines()
+    indent = sys.maxsize
+    for line in lines[1:]:
+        stripped = line.lstrip()
+        if stripped:
+            indent = min(indent, len(line) - len(stripped))
+    trimmed = [lines[0].strip()]
+    if indent < sys.maxsize:
+        for line in lines[1:]:
+            trimmed.append(line[indent:].rstrip())
+    while trimmed and not trimmed[-1]:
+        trimmed.pop()
+    while trimmed and not trimmed[0]:
+        trimmed.pop(0)
+    return '\n'.join(trimmed)
+
+
+def ok_msg(message: str) -> None:
+    """Turns message green."""
+    return str(f'{bcolors.OKGREEN}{trim(message)}{bcolors.ENDC}')
+
+
+def err_msg(message: str) -> None:
+    """Turns message red."""
+    return str(f'{bcolors.FAIL}{trim(message)}{bcolors.ENDC}')
