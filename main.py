f8ec96da1caccd4949ef3072bb76d1dcb8cbab76
Luis Felipe Assmann <luis.assmann.1234@gmail.com>
2024-08-26 09:09:13 -0300

    Select new records to update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.get_sheet_data import get_sheet_data
 from src.get_db_data import fetch_net_records
 import datetime
 
+
 def main():
     list_from_db = fetch_net_records()
     for rec in list_from_db:
@@ -22,8 +23,22 @@ def main():
                                 minute=rec['updated_at'].minute,
                                 second=rec['updated_at'].second
                             ).strftime('%d/%m/%Y %H:%M:%S')
-    p(list_from_db)
-    # list_from_sheet = get_sheet_data()
+    list_from_sheet = get_sheet_data()
+    same = []
+    not_same = []
+    for s_rec in list_from_sheet:
+        for db_rec in list_from_sheet:
+            if db_rec['mac'] == s_rec['mac']:
+                same.append(s_rec)
+            else:
+                if (db_rec['name'] == s_rec['name'] 
+                    and db_rec['device'] == s_rec['device'] 
+                    and db_rec['mac'] != s_rec['mac']):
+                    not_same.append(s_rec)
+    #p(same)
+    p(len(same))
+    p(len(not_same))
+    p(not_same)
 
 
 if __name__ == "__main__":
